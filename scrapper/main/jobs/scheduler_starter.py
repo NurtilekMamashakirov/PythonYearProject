fc08b7ad003eeee6e0de0f73fcffa1206da921a1
Мамашакиров Нуртилек Жанабидинович <nzmamashakirov@edu.hse.ru>
2024-06-09 16:02:40 +0300

    scheduler timer is 20000s

--- scrapper/main/jobs/scheduler_starter.py
+++ scrapper/main/jobs/scheduler_starter.py
@@ -5,5 +5,5 @@ from main.jobs.scheduler import notify_bot_about_updates
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(notify_bot_about_updates, 'interval', seconds=20)
+    scheduler.add_job(notify_bot_about_updates, 'interval', seconds=20000)
     scheduler.start()
