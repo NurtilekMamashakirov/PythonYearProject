def136e2bab231e0d0ed6235e796dcbd66c574c2
Мамашакиров Нуртилек Жанабидинович <nzmamashakirov@edu.hse.ru>
2024-06-09 16:01:08 +0300

    bot is finished

---
+++ bot/main/management/commands/bot.py
@@ -0,0 +1,117 @@
+import requests
+import telebot
+from django.core.management.base import BaseCommand
+
+from main import utils
+from main.config import SCRAPPER_URL, CHATS_PATH, LINKS_PATH
+
+bot = telebot.TeleBot('6699167118:AAGpFnYx282aZ0FLjzb6VfgFXLYjarw8X28')
+
+help_message = ('Доступны следующие комманды: \n '
+                '/start -- запуск бота \n '
+                '/help -- информация о командах \n'
+                '/track -- начать отслеживать ссылку\n'
+                '/untrack -- закончить отслеживание ссылки\n'
+                '/list -- список отслеживаемых ссылок ')
+
+unknown_message = 'Комманда не распознана'
+
+
+class Command(BaseCommand):
+    help = 'Телеграм-бот'
+
+    def handle(self, *args, **options):
+        @bot.message_handler()
+        def message_handler(message: telebot.types.Message):
+            if message.text.lower() == '/start':
+                start_handler(message)
+            elif message.text.lower == '/help':
+                help_handler(message)
+            elif message.text.lower().split(' ')[0] == '/track' and len(message.text.split(' ')) == 2:
+                track_handler(message)
+            elif message.text.lower().split(' ')[0] == '/untrack' and len(message.text.split(' ')) == 2:
+                untrack_handler(message)
+            elif message.text.lower() == '/list':
+                list_handler(message)
+            else:
+                bot.send_message(message.chat.id, unknown_message)
+
+        bot.polling(non_stop=True)
+
+
+def start_handler(message: telebot.types.Message):
+    chat_id = message.chat.id
+    response = requests.get(SCRAPPER_URL + CHATS_PATH)
+    if response.status_code == 200:
+        chats = response.json()['chats']
+        if str(chat_id) in (str(chat['id']) for chat in chats):
+            bot.send_message(chat_id, 'Бот уже запущен')
+        else:
+            bot.send_message(chat_id, f'Добро пожаловать, {message.from_user.first_name}!')
+            requests.post(SCRAPPER_URL + CHATS_PATH, data={'chat_id': chat_id})
+    else:
+        bot.send_message(chat_id, 'Выявлена ошибка. Наша команда разработчиков уже устраняет проблему.')
+
+
+def help_handler(message: telebot.types.Message):
+    bot.send_message(message.chat.id, help_message)
+
+
+def track_handler(message: telebot.types.Message):
+    chat_id = message.chat.id
+    link = message.text.split(' ')[1].lower()
+    if utils.link_is_available(link):
+        response_all_links_of_chat = requests.get(SCRAPPER_URL + LINKS_PATH, params={'chat_id': chat_id})
+        if response_all_links_of_chat.status_code == 200:
+            all_links_of_chat = response_all_links_of_chat.json()
+            if link in (link_of_chat['url'] for link_of_chat in all_links_of_chat):
+                bot.send_message(chat_id, 'Данная ссылка уже отслеживается.')
+            else:
+                response_post_link = requests.post(SCRAPPER_URL + LINKS_PATH, data={'chat_id': chat_id, 'url': link})
+                if response_post_link.status_code == 200:
+                    bot.send_message(chat_id, f'Ссылка {link} успешно добавлена!')
+                else:
+                    bot.send_message(chat_id, f'Выявлена ошибка. Наша команда разработчиков уже устраняет проблему.')
+        else:
+            bot.send_message(chat_id, 'Выявлена ошибка. Наша команда разработчиков уже устраняет проблему.')
+    else:
+        bot.send_message(chat_id,
+                         'Ссылка в неверном формате. Это должна быть ссылка на репозиторий в '
+                         'github или на вопрос в stack overflow.')
+
+
+def untrack_handler(message: telebot.types.Message):
+    chat_id = message.chat.id
+    link = message.text.split(' ')[1].lower()
+    response_all_links_of_chat = requests.get(SCRAPPER_URL + LINKS_PATH, params={'chat_id': chat_id})
+    if response_all_links_of_chat.status_code == 200:
+        all_links_of_chat = response_all_links_of_chat.json()
+        if link in (link_of_chat['url'] for link_of_chat in all_links_of_chat):
+            response_delete_link = requests.delete(SCRAPPER_URL + LINKS_PATH, data={'chat_id': chat_id, 'url': link})
+            if response_delete_link.status_code == 200:
+                bot.send_message(chat_id, f'Ссылка {link} успешно удалена!')
+            else:
+                bot.send_message(chat_id, 'Выявлена ошибка. Наша команда разработчиков уже устраняет проблему.')
+        else:
+            bot.send_message(chat_id, 'Данной ссылки нет в вашем списке.')
+    else:
+        bot.send_message(chat_id, 'Выявлена ошибка. Наша команда разработчиков уже устраняет проблему.')
+
+
+def list_handler(message: telebot.types.Message):
+    chat_id = message.chat.id
+    response_all_links_of_chat = requests.get(SCRAPPER_URL + LINKS_PATH, params={'chat_id': chat_id})
+    if response_all_links_of_chat.status_code == 200:
+        all_links_of_chat = response_all_links_of_chat.json()
+        if len(all_links_of_chat) > 0:
+            list_message_to_user = ''
+            count = 1
+            for link in all_links_of_chat:
+                list_message_to_user += str(count) + '. ' + link['url'] + '\n'
+                count += 1
+            bot.send_message(chat_id, list_message_to_user)
+        else:
+            bot.send_message(chat_id,
+                             'У вас ещё нет ссылок. Вы можете добавить свою первую ссылку с помощью команды /track.')
+    else:
+        bot.send_message(chat_id, 'Выявлена ошибка. Наша команда разработчиков уже устраняет проблему.')
--- bot/main/utils.py
+++ bot/main/utils.py
@@ -1,10 +1,8 @@
 import re
 
 import requests
-from django.test import TestCase
 
 
-# Create your tests here.
 def link_is_available(link: str):
     pattern_gh = r'^https://github.com/[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+$'
     match_gh = re.match(pattern_gh, link)
@@ -13,7 +11,3 @@ def link_is_available(link: str):
     match_sof = re.match(pattern_sof, link)
 
     return (bool(match_gh) or bool(match_sof)) and (requests.get(link).status_code == 200)
-
-
-print(link_is_available(
-    "https://ru.stackoverflow.com/questions/511085/%D0%A7%D1%82%D0%BE-%D1%82%D0%B0%D0%BA%D0%BE%D0%B5-null-pointer-exception-%D0%B8-%D0%BA%D0%B0%D0%BA-%D0%B5%D0%B3%D0%BE-%D0%B8%D1%81%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C"))
