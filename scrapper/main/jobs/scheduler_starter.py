71f22cc23f7e18f946472bd816c8d993d2b37c39
Мамашакиров Нуртилек Жанабидинович <nzmamashakirov@edu.hse.ru>
2024-06-10 20:27:25 +0300

    changed interval to 300s

--- scrapper/main/jobs/scheduler_starter.py
+++ scrapper/main/jobs/scheduler_starter.py
@@ -4,6 +4,11 @@ from main.jobs.scheduler import notify_bot_about_updates
 
 
 def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(notify_bot_about_updates, 'interval', seconds=240)
+    scheduler = BackgroundScheduler({
+        'apscheduler.executors.processpool': {
+            'type': 'processpool',
+            'max_workers': '1'
+        }
+    })
+    scheduler.add_job(notify_bot_about_updates, 'interval', seconds=300)
     scheduler.start()
